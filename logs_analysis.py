3ca711d1791965ffdfa40552f087861dbab79064
Rob Messenger <robmessenger@yahoo.com>
2018-11-13 19:09:54 +0100

    Feat: print a report to the console
    Encapsulate queries in procedures, and call to print the console
    
    Raw timestamp and fraction data from the bad days query are formatted
    nicely
    
    Query calls are currently hardcoded

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,32 +1,61 @@
 #!/usr/bin/env python3
+import psycopg2
 
-top_articles = '''
-    select title, count(*) as num
-        from articles, log
-        where path like '%' || slug
-        group by articles.id
-        order by num desc
-        limit 3; '''
+DBName = 'news'
 
-top_authors = '''
-    select authors.name, count(*) as num
-        from articles, log, authors
-        where path like '%' || slug
-            and author = authors.id
-        group by authors.name
-        order by num desc; '''
+def get_data(query):
+    connection = psycopg2.connect(dbname=DBName)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    connection.close()
+    return data
 
-bad_days = '''
-    select *
-        from
-        (select error_count_daily.date, (cast(errors as real) / cast(requests as real)) as error_frac
+def top_articles():
+    query = '''
+        select title, count(*) as num
+            from articles, log
+            where path like '%' || slug
+            group by articles.id
+            order by num desc
+            limit 3; '''
+    for line in get_data(query):
+        yield line
+
+def top_authors():
+    query = '''
+        select authors.name, count(*) as num
+            from articles, log, authors
+            where path like '%' || slug
+                and author = authors.id
+            group by authors.name
+            order by num desc; '''
+    for line in get_data(query):
+        yield line
+
+def bad_days(tolerance):
+    query = '''
+        select *
             from
-            (select date_trunc('day', time) as date, count(*) as requests
-                from log
-                group by date) as request_count_daily,
-            (select date_trunc('day', time) as date, count(*) as errors
-                from log
-                where not status = '200 OK'
-                group by date) as error_count_daily
-            where error_count_daily.date = request_count_daily.date) as error_frac_daily
-        where error_frac > 0.01; '''
+            (select error_count_daily.date, (cast(errors as real) / cast(requests as real)) as error_frac
+                from
+                (select date_trunc('day', time) as date, count(*) as requests
+                    from log
+                    group by date) as request_count_daily,
+                (select date_trunc('day', time) as date, count(*) as errors
+                    from log
+                    where not status = '200 OK'
+                    group by date) as error_count_daily
+                where error_count_daily.date = request_count_daily.date) as error_frac_daily
+            where error_frac > %s; '''
+    for (timestamptz, error_frac) in get_data(query % tolerance):
+        date = timestamptz.strftime('%B %d, %Y')
+        error_percent = str(round(error_frac * 100, 2)) + '%'
+        yield (date, error_percent)
+
+
+if __name__ == '__main__':
+    #main()
+    #[print(item) for item in top_articles()]
+    #[print(item) for item in top_authors()]
+    [print(item) for item in bad_days(0.01)]
