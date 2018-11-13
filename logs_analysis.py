adeb8ce5aab48ff5b81ce15c800415ce3ecdaf5c
Rob Messenger <robmessenger@yahoo.com>
2018-11-13 17:54:58 +0100

    Add query for bad days
    create a query object that returns bad days (those with >1% errors).
    query validated in psql cli

--- logs_analysis.py
+++ logs_analysis.py
@@ -15,3 +15,18 @@ top_authors = '''
             and author = authors.id
         group by authors.name
         order by num desc; '''
+
+bad_days = '''
+    select *
+        from
+        (select error_count_daily.date, (cast(errors as real) / cast(requests as real)) as error_frac
+            from
+            (select date_trunc('day', time) as date, count(*) as requests
+                from log
+                group by date) as request_count_daily,
+            (select date_trunc('day', time) as date, count(*) as errors
+                from log
+                where not status = '200 OK'
+                group by date) as error_count_daily
+            where error_count_daily.date = request_count_daily.date) as error_frac_daily
+        where error_frac > 0.01; '''
