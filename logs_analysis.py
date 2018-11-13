5184536968a1392fb6aa225e2f9a3ec2463b0291
Rob Messenger <robmessenger@yahoo.com>
2018-11-13 16:25:20 +0100

    Aadd query for top authors
    create a second query object in python to find top authors. query
    validated in psql cli

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
 top_articles = '''
-  select title, count(*) as num
-    from articles, log
-    where path like '%' || slug
-    group by articles.id
-    order by num desc
-    limit 3 '''
+    select title, count(*) as num
+        from articles, log
+        where path like '%' || slug
+        group by articles.id
+        order by num desc
+        limit 3; '''
+
+top_authors = '''
+    select authors.name, count(*) as num
+        from articles, log, authors
+        where path like '%' || slug
+            and author = authors.id
+        group by authors.name
+        order by num desc; '''
